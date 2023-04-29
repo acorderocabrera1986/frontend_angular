cb28956a3da4550d8e7ffec6e90fa9c17367e861
Antonio <acorderocabrera1986@gmail.com>
2023-04-29 12:00:30 -0400

    issue-commit-validacion-personalizada

--- front-end/src/app/generos/crear-genero/crear-genero.component.ts
+++ front-end/src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { primeraLetraMayuscula } from 'src/app/utilidades/validadores/primeraLetraMayuscula';
 
 @Component({
   selector: 'app-crear-genero',
@@ -10,15 +11,34 @@ import { Router } from '@angular/router';
 export class CrearGeneroComponent {
   form: any;
   constructor(private router: Router, private formBuilder: FormBuilder) {
-    form: FormGroup
+    form: FormGroup;
   }
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      nombre : ''
-    })
+      nombre: [
+        '',
+        {
+          validators: [Validators.required, Validators.minLength(3), primeraLetraMayuscula()],
+        },
+      ],
+    });
   }
   guardarCambio() {
     //..guardar los cambios
-    this.router.navigate(['/generos'])
+    this.router.navigate(['/generos']);
+  }
+
+  obtenerErrorCampoNombre() {
+    var campo = this.form.get('nombre');
+    if (campo.hasError('required')) {
+      return 'El campo nombre es requerido';
+    }
+    if (campo.hasError('minlength')) {
+      return 'La longitud mínima es de 3 caracteres';
+    }
+    if (campo.hasError('primeraLetraMayuscula')) {
+      return campo.getError('primeraLetraMayuscula').mensaje;
+    }
+    return '';
   }
 }
