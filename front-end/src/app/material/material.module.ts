5a6116d587800d1da783b2f413de09b760a0884a
Antonio <acorderocabrera1986@gmail.com>
2023-05-08 23:20:47 -0400

    issue-commit-filtro-peliculas

--- front-end/src/app/material/material.module.ts
+++ front-end/src/app/material/material.module.ts
@@ -5,10 +5,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 
 @NgModule({
   declarations: [],
-  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatCheckboxModule],
   imports: [CommonModule],
 })
 export class MaterialModule {}
