d53e07cd0ebec8b9b5c901b64d139cd1e9854426
Antonio <acorderocabrera1986@gmail.com>
2023-05-10 23:13:24 -0400

    issue-commit-filtro-de-peliculas-query-strings

--- front-end/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
+++ front-end/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-filtro-peliculas',
@@ -7,7 +9,11 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./filtro-peliculas.component.css'],
 })
 export class FiltroPeliculasComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private location: Location,
+    private activatedRoute: ActivatedRoute
+  ) {}
   form: FormGroup | any;
   generos = [
     { id: 1, nombre: 'Drama' },
@@ -49,31 +55,79 @@ export class FiltroPeliculasComponent implements OnInit {
     generoId: 0,
     proximosEstrenos: false,
     enCines: false,
-  }
+  };
   ngOnInit(): void {
     this.form = this.formBuilder.group(this.formularioOriginal);
+    this.leerValoresURL();
+    this.buscarPeliculas(this.form.value);
     this.form.valueChanges.subscribe((valores: any) => {
       this.peliculas = this.peliculasOriginal;
-      this.buscarPeliculas(valores)
+      this.buscarPeliculas(valores);
+      this.escribirParametrosBusquedaURL();
     });
   }
 
-  buscarPeliculas(valores:any){
-    if(valores.titulo){
-      this.peliculas = this.peliculas.filter(pelicula => pelicula.titulo.indexOf(valores.titulo)!== -1)
+  private leerValoresURL() {
+    this.activatedRoute.queryParams.subscribe((params: any) => {
+      var objeto: any = {};
+      if (params.titulo) {
+        objeto.titulo = params.titulo;
+      }
+      if (params.generoId) {
+        objeto.generoId = Number(params.generoId);
+      }
+      if (params.proximosEstrenos) {
+        objeto.proximosEstrenos = params.proximosEstrenos;
+      }
+      if (params.enCines) {
+        objeto.enCines = params.enCines;
+      }
+      this.form.patchValue(objeto);
+    });
+  }
+
+  private escribirParametrosBusquedaURL() {
+    var queryStrings = [];
+    var valoresFormulario = this.form.value;
+    if (valoresFormulario.titulo) {
+      queryStrings.push(`titulo=${valoresFormulario.titulo}`);
     }
-    if(valores.generoId !== 0){
-      this.peliculas = this.peliculas.filter(pelicula => pelicula.generos.indexOf(valores.generoId)!== -1)
+    if (valoresFormulario.generoId != '0') {
+      queryStrings.push(`generoId=${valoresFormulario.generoId}`);
     }
-    if(valores.proximosEstrenos){
-      this.peliculas = this.peliculas.filter(pelicula => pelicula.proximosEstrenos)
+    if (valoresFormulario.proximosEstrenos) {
+      queryStrings.push(
+        `proximosEstrenos=${valoresFormulario.proximosEstrenos}`
+      );
     }
-    if(valores.enCines){
-      this.peliculas = this.peliculas.filter(pelicula => pelicula.enCines)
+    if (valoresFormulario.enCines) {
+      queryStrings.push(`enCines=${valoresFormulario.enCines}`);
+    }
+    this.location.replaceState('peliculas/buscar', queryStrings.join('&'));
+  }
+
+  buscarPeliculas(valores: any) {
+    if (valores.titulo) {
+      this.peliculas = this.peliculas.filter(
+        (pelicula) => pelicula.titulo.indexOf(valores.titulo) !== -1
+      );
+    }
+    if (valores.generoId !== 0) {
+      this.peliculas = this.peliculas.filter(
+        (pelicula) => pelicula.generos.indexOf(valores.generoId) !== -1
+      );
+    }
+    if (valores.proximosEstrenos) {
+      this.peliculas = this.peliculas.filter(
+        (pelicula) => pelicula.proximosEstrenos
+      );
+    }
+    if (valores.enCines) {
+      this.peliculas = this.peliculas.filter((pelicula) => pelicula.enCines);
     }
   }
 
   limpiar() {
-    this.form.patchValue(this.formularioOriginal)
+    this.form.patchValue(this.formularioOriginal);
   }
 }
